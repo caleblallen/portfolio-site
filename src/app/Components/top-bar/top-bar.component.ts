46487aef98e209be26f5a78deb8dc62a7c6a33a7
Caleb Allen <caleb@caleballen.com>
2019-10-20 16:33:07 -0700

    Getting the website blocked out. Working through how to get a few things done w/ angular.

---
+++ src/app/Components/top-bar/top-bar.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.css'],
+})
+export class TopBarComponent implements OnInit {
+  public test: string;
+  private menus: {};
+  private keys: string[];
+  constructor() {
+    this.test = '123';
+  }
+
+  ngOnInit() {
+/*    this.menus = {
+      Portfolio: [['item1', 'href'], ['item2', 'href']]
+    };*/
+    // this.menus = [['item1', 'href'], ['item2', 'href']];
+    this.menus = {
+      Portfolio: [
+        {
+          title: 'Item1',
+          link: 'href://'
+        },
+        {
+          title: 'Item2',
+          link: 'href://'
+        }
+      ],
+      Blog: [
+        {}
+      ],
+      Other: [
+        {}
+      ]
+    };
+    this.keys = Object.keys(this.menus);
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule} from '@angular/material';
+import { TopBarComponent } from './Components/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
