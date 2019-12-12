90ffb233587cd7aed95da2e297a47ee610a4a4cc
Caleb Allen <Caleb.Allen@GMail.com>
2019-12-12 09:32:54 -0800

    File replacements were working for angular, but webpack module replacements were not configured. Found and changed.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  port: 66000,
+  port: 65001,
   certificate: {
     key: '/etc/letsencrypt/live/caleballen.com/privkey.pem',
     cert: '/etc/letsencrypt/live/caleballen.com/fullchain.pem'
