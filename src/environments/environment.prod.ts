507cdd063e63db15e1862deebb2abbddfd7207e3
Caleb Allen <Caleb.Allen@GMail.com>
2019-12-12 09:33:28 -0800

    fixed port number in config file.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  port: 65001,
+  port: 65000,
   certificate: {
     key: '/etc/letsencrypt/live/caleballen.com/privkey.pem',
     cert: '/etc/letsencrypt/live/caleballen.com/fullchain.pem'
